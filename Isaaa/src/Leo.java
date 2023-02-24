560e61753402c65d61e473a0f05bd6db9eb5d374
CAMSON COLACO <CAMSON COLACO@LAPTOP-V3OJ0C51>
2023-02-24 21:41:38 +0530

    jxhhf

---
+++ Isaaa/src/Leo.java
@@ -0,0 +1,12 @@
+
+public class Leo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Sir");
+		System.out.println("Hello Mam");
+
+	}
+
+}
